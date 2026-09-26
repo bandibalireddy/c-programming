52368b602bd6e3f19138d68d1141c73608ebe3f1
bandibalireddy <parisatyabalireddybandi@gmail.com>
2026-09-26 16:31:44 +0530

    Intial commit

--- functions/functions.c
+++ functions/functions.c
@@ -1,14 +1,41 @@
-#include <stdio.h>
-void greet()
-{
-    printf("Hello\n");
-    printf("how are you?\n");
-    return;
-}
-int main()
-{
-    greet();
-    greet();
-    return 0;
+// #include <stdio.h>
+// void greet()
+// {
+//     printf("Hello\n");
+//     printf("how are you?\n");
+//     return;
+// }
+// int main()
+// {
+//     greet();
+//     greet();
+//     return 0;
 
-}
+// }
+
+// #include <stdio.h>
+// int add(int a, int b)
+// {
+//     return a + b;
+// }
+// int main()
+// {
+//     int sum;
+//     sum = add(5, 6);
+//     printf("%d", sum);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// #include <math.h>
+// int main()
+// {
+//     int n;
+//     printf("Enter a number : ");
+//     scanf("%d", &n);
+//     int i = sqrt(n);
+//     printf("%d\n", i);
+//     int r = pow(n,n);
+//     printf("%d\n", r);
+//     return 0;
+// }
